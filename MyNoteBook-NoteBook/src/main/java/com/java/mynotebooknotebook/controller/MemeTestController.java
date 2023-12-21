f0ee5569e3b573fd6f2f154f5b349441c8707a18
tanlin123 <534606744@qq.com>
2023-12-21 19:34:48 +0800

    react test --Glenn --20231221

--- MyNoteBook-NoteBook/src/main/java/com/java/mynotebooknotebook/controller/MemeTestController.java
+++ MyNoteBook-NoteBook/src/main/java/com/java/mynotebooknotebook/controller/MemeTestController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
@@ -14,14 +15,26 @@ public class MemeTestController {
     @GetMapping("/memes/getList")
     public Result getList() {
         HashMap<String, Object> memes = new HashMap<>();
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("id", 1);
-        map.put("name", "Drake");
-        map.put("https", "https://img0.baidu.com/it/u=1524510428,2480306645&fm=253&fmt=auto&app=138&f=PNG?w=280&h=350");
-        map.put("width", 1200);
-        map.put("height", 1200);
-        map.put("box_count", 1);
-        memes.put("memes", map);
+        HashMap<String, Object> map1 = new HashMap<>();
+        HashMap<String, Object> map2 = new HashMap<>();
+        ArrayList<HashMap> list = new ArrayList<>();
+        map1.put("id", 1);
+        map1.put("name", "Drake");
+        map1.put("url", "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fb1a914be-4e3d-47ca-9406-780e9530aa9c%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1705746402&t=1fce3b2a8ae6ad843d4c1353ce86a0c0");
+        map1.put("width", 1200);
+        map1.put("height", 1200);
+        map1.put("box_count", 1);
+
+        map2.put("id", 2);
+        map2.put("name", "Luke");
+        map2.put("url", "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F12f18496-3075-4367-b599-ae390f0e0b37%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1705746269&t=990a2d183aea29d07f93bc97868bd155");
+        map2.put("width", 1200);
+        map2.put("height", 1200);
+        map2.put("box_count", 2);
+
+        list.add(map1);
+        list.add(map2);
+        memes.put("memes", list);
         System.out.println(memes);
         return Result.success(memes);
     }
